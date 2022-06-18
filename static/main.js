6078090e953e5bf44d45934c800b0d13977e639b
Mandyiee <chiomzibae101@gmail.com>
2022-06-18 19:47:23 +0100

    first commit

---
+++ static/main.js
@@ -0,0 +1,12 @@
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.querySelector("header").style.height = "10vh";
+    
+  } 
+  else {
+    document.querySelector("header").style.height= "50vh";
+ 
+  }
+}
